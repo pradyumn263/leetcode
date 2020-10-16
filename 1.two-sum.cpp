211ac4c22fb16979118ace80fa4470bb0c960f29
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-10-16 11:42:02 +0530

    Lets Start Again :)

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,7 +8,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.find(target - nums[i]) == mp.end()) {
+                mp[nums[i]] = i;
+            } else {
+                vector<int> res;
+                res.push_back(mp[target - nums[i]]);
+                res.push_back(i);
+
+                return res;
+            }
+        }
+        return {};
     }
 };
 // @lc code=end
